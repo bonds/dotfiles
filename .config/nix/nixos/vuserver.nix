2c299f8a05e180cf9bd8aadd38f4d28b855dd1a9
Scott Bonds <scott@ggr.com>
2024-09-10 11:12:00 -0700

    rename vuserver device to include specific model

--- .config/nix/nixos/vuserver.nix
+++ .config/nix/nixos/vuserver.nix
@@ -44,15 +44,36 @@ in {
 
     # start service when the VU1 is plugged in and stop when it is unplugged
     # and pass the device path to the service
+    # services.udev.extraRules = ''
+    #   ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", TAG+="systemd", SYMLINK+="myspecialdevice", ENV{SYSTEMD_WANTS}="myservice@myspecialdevice.service"
+    #   ACTION=="remove", SUBSYSTEM=="usb", ENV{PRODUCT}=="0403/6015/*", RUN+="${pkgs.systemd}/bin/systemctl stop myservice@myspecialdevice.service"
+    # '';
+
+    # services.udev.extraRules = ''
+    #   ACTION=="add", SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", SYMLINK+="myspecialdevice", TAG+="systemd", ENV{SYSTEMD_WANTS}="myservice@myspecialdevice.service"
+    #   ACTION=="remove", SUBSYSTEM=="tty", ENV{ID_VENDOR_ID}=="0403", ENV{ID_MODEL_ID}=="6015", RUN+="${pkgs.systemd}/bin/systemctl stop myservice@myspecialdevice.service"
+    # '';
+
+    # services.udev.extraRules = ''
+    #   ACTION=="add", SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", SYMLINK+="myspecialdevice", TAG+="systemd", ENV{SYSTEMD_WANTS}="vuserver@myspecialdevice.service", RUN+="/run/current-system/sw/bin/logger -t udevrule 'Triggering vuserver for myspecialdevice'"
+    #   ACTION=="remove", SUBSYSTEM=="tty", ENV{ID_VENDOR_ID}=="0403", ENV{ID_MODEL_ID}=="6015", RUN+="${pkgs.systemd}/bin/systemctl stop vuserver@myspecialdevice.service"
+    # '';
+
+    # services.udev.extraRules = ''
+    #   ACTION=="add", SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", SYMLINK+="myspecialdevice", TAG+="systemd", ENV{SYSTEMD_WANTS}="vuserver@myspecialdevice.service", RUN+="${pkgs.systemd}/bin/systemd-cat -t udevrule echo 'Triggering vuserver for myspecialdevice'"
+    #   ACTION=="remove", SUBSYSTEM=="tty", ENV{ID_VENDOR_ID}=="0403", ENV{ID_MODEL_ID}=="6015", RUN+="${pkgs.systemd}/bin/systemctl stop vuserver@myspecialdevice.service"
+    # '';
+
     services.udev.extraRules = ''
-      ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", TAG+="systemd", SYMLINK+="myspecialdevice", ENV{SYSTEMD_WANTS}="myservice@myspecialdevice.service"
-      ACTION=="remove", SUBSYSTEM=="usb", ENV{PRODUCT}=="0403/6015/*", RUN+="${pkgs.systemd}/bin/systemctl stop myservice@myspecialdevice.service"
+      ACTION=="add", SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", ATTRS{serial}=="DQ0164KM", SYMLINK+="vuserver-$attr{serial}", TAG+="systemd", ENV{SYSTEMD_WANTS}="vuserver@$attr{serial}.service"
+      ACTION=="remove", SUBSYSTEM=="tty", ENV{ID_VENDOR_ID}=="0403", ENV{ID_MODEL_ID}=="6015", ENV{ID_SERIAL_SHORT}=="DQ0164KM", RUN+="${pkgs.systemd}/bin/systemctl stop vuserver@$env{ID_SERIAL_SHORT}.service"
     '';
 
     systemd.services."vuserver@" = {
       description = "VU Server for %I. Provides API, admin web page, and pushed updates to USB dials";
 
       serviceConfig = {
+        # ExecStart = "${pkgs.vuserver}/bin/vuserver /dev/vuserver-%I";
         ExecStart = "${pkgs.vuserver}/bin/vuserver";
         User = cfg.user;
         Group = cfg.group;
@@ -67,7 +88,7 @@ in {
           "STATEDIR=%S/vuserver"
           "LOGSDIR=%L/vuserver"
           "RUNTIMEDIR=%t/vuserver"
-          "DEVICE=/dev/%I"
+          "DEVICE=/dev/vuserver-%I"
           "CONFIG=\"{'hostname': 'localhost', 'port': ${toString cfg.port},'communication_timeout': 10, 'master_key': ${cfg.key}}\""
         ];
       };
