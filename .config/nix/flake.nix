020015262a84a0fc4fceb0a33c1c7b267fef8ead
Scott Bonds <scott@ggr.com>
2026-05-17 11:19:48 -0700

    improve flake: dedup pkgs-unstable, drop isDarwin, add devShell, home-manager for sophrosyne, arion follows stable

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -10,6 +10,7 @@
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     arion.url = "github:hercules-ci/arion/v0.2.2.0";
+    arion.inputs.nixpkgs.follows = "nixpkgs-stable";
     vudials.url = "github:bonds/nix-vudials";
   };
   outputs = inputs @ {
@@ -22,6 +23,10 @@
     arion,
     vudials,
   }: let
+    pkgs-unstable = import nixpkgs {
+      system = "x86_64-linux";
+      config = {allowUnfree = true;};
+    };
     darwinPkgs = import nixpkgs {
       system = "aarch64-darwin";
       config = {allowUnfree = true;};
@@ -35,12 +40,19 @@
       aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.alejandra;
       x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
     };
-    darwinConfigurations."accismus" = nix-darwin.lib.darwinSystem {
+
+    devShells.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.mkShell {
+      packages = [nixpkgs.legacyPackages.aarch64-darwin.alejandra];
+    };
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      packages = with nixpkgs.legacyPackages.x86_64-linux; [alejandra nh];
+    };
+
+    darwinConfigurations.accismus = nix-darwin.lib.darwinSystem {
       specialArgs = {
         inherit self inputs;
         vuserver = darwinPkgs.callPackage "${vudials}/pkgs/vuserver" {};
         vuclient = darwinPkgs.callPackage "${vudials}/pkgs/vuclient" {};
-        isDarwin = true;
       };
       modules = [
         {nixpkgs.overlays = [inputs.nix-index-database.overlays.nix-index];}
@@ -66,10 +78,8 @@
     nixosConfigurations.sophrosyne = nixpkgs-stable.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {
-        pkgs-unstable = import nixpkgs {
-          system = "x86_64-linux";
-          config = {allowUnfree = true;};
-        };
+        inherit inputs;
+        pkgs-unstable = pkgs-unstable;
       };
       modules = [
         ./hosts/sophrosyne/configuration.nix
@@ -77,6 +87,17 @@
         arion.nixosModules.arion
         nix-index-database.nixosModules.nix-index
         ./modules/fish-command-not-found.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "old";
+          home-manager.users.scott = {pkgs, ...}: {
+            home.stateVersion = "24.11";
+            home.homeDirectory = "/home/scott";
+            programs.fish.plugins = with pkgs.fishPlugins; [fzf-fish];
+          };
+        }
       ];
     };
     nixosConfigurations.metanoia = nixpkgs-stable.lib.nixosSystem {
@@ -85,11 +106,7 @@
         inherit inputs;
         vuserver = linuxStablePkgs.callPackage "${vudials}/pkgs/vuserver" {};
         vuclient = linuxStablePkgs.callPackage "${vudials}/pkgs/vuclient" {};
-        isDarwin = false;
-        pkgs-unstable = import nixpkgs {
-          system = "x86_64-linux";
-          config = {allowUnfree = true;};
-        };
+        pkgs-unstable = pkgs-unstable;
       };
       modules = [
         ./hosts/metanoia/configuration.nix
