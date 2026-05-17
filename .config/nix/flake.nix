efac95adaae1a909da016488a45fe82c2adf3343
Scott Bonds <scott@ggr.com>
2026-05-17 11:32:34 -0700

    remove home-manager nixpkgs follows — incompatible with stable nixpkgs on sophrosyne

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -6,7 +6,6 @@
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     arion.url = "github:hercules-ci/arion/v0.2.2.0";
