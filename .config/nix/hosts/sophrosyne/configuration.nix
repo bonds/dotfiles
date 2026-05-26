7afc3d18d270852aba3e2be78b0fb59c28eecad4
Scott Bonds <scott@ggr.com>
2026-05-25 17:45:03 -0700

    sophrosyne: go headless — remove X11, GNOME, Firefox, printing, PipeWire, ghostty

--- .config/nix/hosts/sophrosyne/configuration.nix
+++ .config/nix/hosts/sophrosyne/configuration.nix
@@ -42,28 +42,7 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  services.xserver.enable = true;
-
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "dvorak";
-  };
-
   console.keyMap = "dvorak";
-
-  services.printing.enable = true;
-
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
 
   users.users.scott = {
     isNormalUser = true;
@@ -72,13 +51,10 @@
     packages = with pkgs; [];
   };
 
-  programs.firefox.enable = true;
-
   environment.systemPackages = with pkgs; [
     nvme-cli
     util-linux
     docker-compose
-    ghostty
     dmidecode
     edac-utils
     # most common packages are in modules/packages/common.nix
