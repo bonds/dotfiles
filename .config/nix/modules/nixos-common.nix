3e5c26df287bdad4ac035e29078bb8deab303056
Scott Bonds <scott@ggr.com>
2026-06-03 17:18:57 -0700

    Remove auto-allocate-uids and use-cgroups (unnecessary for single-user)

--- .config/nix/modules/nixos-common.nix
+++ .config/nix/modules/nixos-common.nix
@@ -12,9 +12,7 @@
     channel.enable = false;
     registry = lib.mapAttrs (_: flake: lib.mkDefault {inherit flake;}) flakeInputs;
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-    settings.experimental-features = lib.mkDefault "nix-command flakes auto-allocate-uids";
-    settings.auto-allocate-uids = lib.mkDefault true;
-    settings.use-cgroups = lib.mkDefault true;
+    settings.experimental-features = lib.mkDefault "nix-command flakes";
   };
 
   time.timeZone = "America/Los_Angeles";
