62a40d4ee7a29c2ace539bf49ccdc89102c124d8
Scott Bonds <scott@ggr.com>
2026-06-10 13:02:13 -0700

    accismus: use postInstall hook for pam_reattach library patch

--- .config/nix/modules/pam-reattach-overlay.nix
+++ .config/nix/modules/pam-reattach-overlay.nix
@@ -1,7 +1,7 @@
 final: prev: {
   pam-reattach = prev.pam-reattach.overrideAttrs (old: {
-    postFixup =
-      (old.postFixup or "")
+    postInstall =
+      (old.postInstall or "")
       + ''
         install_name_tool -change ${prev.openpam}/lib/libpam.2.dylib /usr/lib/libpam.2.dylib $out/lib/pam/pam_reattach.so
       '';
