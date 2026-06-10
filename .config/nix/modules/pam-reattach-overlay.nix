98c8f6b68630960c1ab07b0d5e49a0a27becdb58
Scott Bonds <scott@ggr.com>
2026-06-10 12:59:36 -0700

    accismus: drop codesign from pam-reattach overlay (not in sandbox)

--- .config/nix/modules/pam-reattach-overlay.nix
+++ .config/nix/modules/pam-reattach-overlay.nix
@@ -4,7 +4,6 @@ final: prev: {
       (old.postFixup or "")
       + ''
         install_name_tool -change ${prev.openpam}/lib/libpam.2.dylib /usr/lib/libpam.2.dylib $out/lib/pam/pam_reattach.so
-        codesign -f -s - $out/lib/pam/pam_reattach.so
       '';
   });
 }
