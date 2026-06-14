6fb6ad2fe8eb9b420ab3aac7ffe1e952bc5769d8
Scott Bonds <scott@ggr.com>
2026-06-14 14:57:33 -0700

    fix: pin home-manager-stable to release-26.05 branch to match nixpkgs-stable

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -7,7 +7,7 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager-stable.url = "github:nix-community/home-manager";
+    home-manager-stable.url = "github:nix-community/home-manager/release-26.05";
     home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
