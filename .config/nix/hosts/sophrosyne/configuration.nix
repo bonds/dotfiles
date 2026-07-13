f895d325d45174fb3d6b57e8873392c39cfc3231
Scott Bonds <scott@ggr.com>
2026-07-12 18:13:52 -0700

    sophrosyne: enable Avahi mDNS with Xserve model for Finder icon

--- .config/nix/hosts/sophrosyne/configuration.nix
+++ .config/nix/hosts/sophrosyne/configuration.nix
@@ -192,7 +192,39 @@
     interval = "*-*-01 03:00:00";
   };
 
-  services.avahi.enable = false;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+    };
+    extraServiceFiles = {
+      smb = ''
+        <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+        <service-group>
+          <name replace-wildcards="yes">%h</name>
+          <service>
+            <type>_smb._tcp</type>
+            <port>445</port>
+          </service>
+        </service-group>
+      '';
+      device-info = ''
+        <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+        <service-group>
+          <name replace-wildcards="yes">%h</name>
+          <service>
+            <type>_device-info._tcp</type>
+            <port>0</port>
+            <txt-record>model=Xserve</txt-record>
+          </service>
+        </service-group>
+      '';
+    };
+  };
 
   services.samba = {
     enable = true;
