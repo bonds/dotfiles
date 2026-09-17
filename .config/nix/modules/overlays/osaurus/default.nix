2345383f0f7e672a0fe88be3fafcb320b90ce24c
Scott Bonds <scott@ggr.com>
2026-09-16 19:57:39 -0700

    nr --update: bump nightly dependency versions

--- .config/nix/modules/overlays/osaurus/default.nix
+++ .config/nix/modules/overlays/osaurus/default.nix
@@ -1,13 +1,13 @@
 final: prev: {
   osaurus = final.mkDarwinPackage rec {
     pname = "osaurus";
-    # version = "0.25.4";
-    version = "0.25.4";
+    # version = "0.25.5";
+    version = "0.25.5";
 
     src = prev.fetchurl {
       url = "https://github.com/osaurus-ai/osaurus/releases/download/${version}/Osaurus-${version}.dmg";
-      # hash = "sha256-d579o1JZL2kHwxsE4/0dq0jtGAITOfokaqcLR7J+gCw=";
-      hash = "sha256-d579o1JZL2kHwxsE4/0dq0jtGAITOfokaqcLR7J+gCw=";
+      # hash = "sha256-gUe+pp6fXo3OqPI9M8dJiQVMgPSVoSs8T22mRbniO3E=";
+      hash = "sha256-gUe+pp6fXo3OqPI9M8dJiQVMgPSVoSs8T22mRbniO3E=";
     };
 
     nativeBuildInputs = [prev._7zz];
